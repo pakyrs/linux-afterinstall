74b844c75bf9011ddccf767b2ec8561139db1d3e
pakyrs <56080974+pakyrs@users.noreply.github.com>
2021-12-18 18:56:17 +0000

    Update multi-distro.sh

--- multi-distro.sh
+++ multi-distro.sh
@@ -14,5 +14,58 @@ do
 done
 
 package="git"
+package="git"
+       - vlc
+       - bleachbit
+       - cheese
+       - cmatrix
+       - curl
+       - figlet
+       - filezilla
+       - flameshot
+       - fontconfig
+       - gimp
+       - git
+       - gnupg
+       - gparted
+       - guake
+       - htop
+       - iftop
+       - inkscape
+       - ipcalc
+       - iperf
+       - kitty
+       - keepassxc
+       - plank
+       - lshw
+       - meld
+       - mtr
+       - mosh
+       - ncdu
+       - neofetch
+       - net-tools
+       - nmap
+       - p7zip
+       - powerline
+       - pwgen
+       - qbittorrent
+       - ranger
+       - redshift
+       - remmina
+       - screen
+       - scribus
+       - shotwell
+       - speedtest-cli
+       - sshuttle
+       - telnet
+       - tldr
+       - tmate
+       - tmux
+       - tree
+       - vim
+       - virt-manager
+       - wget
+       - wine
+       - zsh
 
 ${package_manager} ${package}
